d318f8b54ac367754eccf5ed938bb7d1730c278b
Urvisha <ramaniurvi1@gmail.com>
2022-08-06 10:43:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.brown,
+      body: Text("hello this is test",
+            style: TextStyle(
+               color: Colors.lightGreen,
+                fontSize: 30,
+                backgroundColor: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                letterSpacing: 5 ,
+                wordSpacing: 10,
+            ),
+      ),
+      appBar: AppBar(
+        title: Text("My App",style: TextStyle(color: Colors.black,fontSize: 30),),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
+
+      ),
+    ),   
+  ));
+}
